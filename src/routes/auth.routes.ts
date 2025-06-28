a5a4b82d1fffab334bed777cde2dfc00e64e0e02
Cam Johnson <cameron.n.johnson15@gmail.com>
2025-06-28 10:22:59 -0600

    Add route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -3,8 +3,7 @@ import { authController } from '../controllers/auth.controller';
 
 const router = Router();
 
-router.post('/login', authController.login);
 router.get('/callback', authController.handleCallback);
-router.post('/refresh', authController.refreshToken);
+router.get('/downloading-activities', authController.handleDownloadingActivities);
 
 export default router;
